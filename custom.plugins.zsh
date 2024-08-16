fb8f8c47a2a80482a265a6a308dda16e285ad5e9
Xueyuan Jiang <alexxyjiang@gmail.com>
2024-08-16 10:58:08 +0800

    fix custom script

--- custom.plugins.zsh
+++ custom.plugins.zsh
@@ -1,7 +1,8 @@
 #!/bin/zsh 
 # install my custom plugins
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
-git clone https://github.com/popstas/zsh-command-time.git $ZSH_CUSTOM/plugins/command-time
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+export ZSH_CUSTOM="${ZSH}/custom"
+[ -d "${ZSH_CUSTOM}/plugins/you-should-use" ] || git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM}/plugins/you-should-use
+[ -d "${ZSH_CUSTOM}/plugins/autoupdate" ] || git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM}/plugins/autoupdate
+[ -d "${ZSH_CUSTOM}/plugins/command-time" ] || git clone https://github.com/popstas/zsh-command-time.git ${ZSH_CUSTOM}/plugins/command-time
+[ -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ] || git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+[ -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
